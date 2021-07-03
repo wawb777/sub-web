e515cbd09a3eaa24aa4285d37ad65a50fe30f0ac
wawb777 <52372297+wawb777@users.noreply.github.com>
2021-07-03 13:18:27 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -256,7 +256,7 @@ export default {
         },
         customBackend: {
           "localhost:25500 本地版": "http://localhost:25500/sub?",
-          "ww3.xuexinlp01.ml": "https://ww3.xuexinlp01.ml/sub?",
+          "ww1.chenzhan.work": "https://ww1.chenzhan.work/sub?",
           "subcon.dlj.tf(subconverter作者提供-稳定)":
             "https://subcon.dlj.tf/sub?",
           "api.dler.io(sub作者&lhie1提供-稳定)": "https://api.dler.io/sub?",
@@ -265,7 +265,7 @@ export default {
         },
         backendOptions: [
           { value: "http://localhost:25500/sub?" },
-          { value: "https://ww3.xuexinlp01.ml/sub?" },
+          { value: "https://ww1.chenzhan.work/sub?" },
           { value: "https://subcon.dlj.tf/sub?" },
           { value: "https://api.dler.io/sub?" },
           { value: "https://api.wcc.best/sub?" },
